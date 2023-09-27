b117fb762cc63ae6c9a9d456a37d34698d1404a0
austineKE <austinewamalwa18@gmail.com>
2023-09-28 00:36:00 +0300

    initialized the project

---
+++ src/main/java/co/sys/procurement/tmsswiftserver/TmsSwiftServerApplication.java
@@ -0,0 +1,13 @@
+package co.sys.procurement.tmsswiftserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TmsSwiftServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TmsSwiftServerApplication.class, args);
+	}
+
+}
